8551a575496226214b555418d699d2b449a625e2
yzc <yzc@qq.com>
2023-07-24 20:30:48 +0800

    add

---
+++ run.sh
@@ -0,0 +1,3 @@
+python main --config domain_shift_semi/1_7/uda1/G1R7R4_semi_scratch_tgt_label_multi_prototype
+python main --config domain_shift_semi/1_7/uda1/G1R7R4_semi_Pretrained_prototype_multi_prototype
+/usr/bin/shutdown
