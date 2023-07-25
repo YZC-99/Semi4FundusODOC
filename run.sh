9981a8476f4b6ac90e02c6461b346860d0dc051f
yzc <yzc@qq.com>
2023-07-25 21:09:59 +0800

    add

--- run.sh
+++ run.sh
@@ -1,3 +1,13 @@
-python main --config domain_shift_semi/1_7/uda1/G1R7R4_semi_scratch_tgt_label_multi_prototype
-python main --config domain_shift_semi/1_7/uda1/G1R7R4_semi_Pretrained_prototype_multi_prototype
-/usr/bin/shutdown
+# No Strong
+python main --config domain_shift_semi/1_7/strong1/G1R7R4_None_semi
+python main --config domain_shift_semi/1_7/strong1/G1R7R4_B_CJ_CO_RG_semi
+python main --config domain_shift_semi/1_7/strong1/G1R7R4_B_CJ_CO_semi
+python main --config domain_shift_semi/1_7/strong1/G1R7R4_CJ_CO_semi
+
+# uda
+python main --config domain_shift_semi/1_7/uda1/G1R7R4_None_semi_tgt_label
+python main --config domain_shift_semi/1_7/uda1/G1R7R4_B_CJ_CO_RG_semi_tgt_label
+python main --config domain_shift_semi/1_7/uda1/G1R7R4_B_CJ_CO_semi_tgt_label
+python main --config domain_shift_semi/1_7/uda1/G1R7R4_B_CJ_semi_tgt_label
+python main --config domain_shift_semi/1_7/uda1/G1R7R4_CJ_CO_semi_tgt_label
+
