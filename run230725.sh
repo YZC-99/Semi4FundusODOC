3933417ca86171d6252a4c14ea6784ba68ba0103
yzc <yzc@qq.com>
2023-07-25 21:19:34 +0800

    add

---
+++ run230725.sh
@@ -0,0 +1,13 @@
+# No Strong
+python main.py --config domain_shift_semi/1_7/strong1/G1R7R4_None_semi
+python main.py --config domain_shift_semi/1_7/strong1/G1R7R4_B_CJ_CO_RG_semi
+python main.py --config domain_shift_semi/1_7/strong1/G1R7R4_B_CJ_CO_semi
+python main.py --config domain_shift_semi/1_7/strong1/G1R7R4_CJ_CO_semi
+
+# uda
+python main.py --config domain_shift_semi/1_7/uda1/G1R7R4_None_semi_tgt_label
+python main.py --config domain_shift_semi/1_7/uda1/G1R7R4_B_CJ_CO_RG_semi_tgt_label
+python main.py --config domain_shift_semi/1_7/uda1/G1R7R4_B_CJ_CO_semi_tgt_label
+python main.py --config domain_shift_semi/1_7/uda1/G1R7R4_B_CJ_semi_tgt_label
+python main.py --config domain_shift_semi/1_7/uda1/G1R7R4_CJ_CO_semi_tgt_label
+
