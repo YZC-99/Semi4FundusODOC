557de3107c8ae29591599fc0f54d477fd3c18cf6
yzc <yzc@qq.com>
2023-07-27 21:26:46 +0800

    add

--- run230727.sh
+++ run230727.sh
@@ -1,5 +1,13 @@
 # SEG
 python main.py --config SEG/sup/random1_ODOC_sup
 python main.py --config SEG/sup/random1_sup10
+python main.py --config SEG/sup/random1_sup20
+python main.py --config SEG/sup/random1_sup30
+python main.py --config SEG/sup/random1_sup40
+python main.py --config SEG/sup/random1_sup50
+python main.py --config SEG/sup/random1_sup60
+python main.py --config SEG/sup/random1_sup70
+python main.py --config SEG/sup/random1_sup80
+python main.py --config SEG/sup/random1_sup90
 
 
