836a4422cd78d6695df52f3c23b5e389a474d6b9
yzc <yzc@qq.com>
2023-07-28 20:41:42 +0800

    add

--- run230728.sh
+++ run230728.sh
@@ -13,5 +13,22 @@ python main.py --config SEG/semi/10/ODOC_semi90
 #sup align
 CUDA_VISIBLE_DEVICES=4,5 python main.py --config SEG/sup/random1_ODOC_sup_align3e-1
 
+CUDA_VISIBLE_DEVICES=0,1 python main.py --config SEG/sup/random1_ODOC_sup_align1e-1 &
+CUDA_VISIBLE_DEVICES=2,3 python main.py --config SEG/sup/random1_ODOC_sup_align2e-1 &
+CUDA_VISIBLE_DEVICES=4,5 python main.py --config SEG/sup/random1_ODOC_sup_align4e-1 &
+PID1=$!
+
+wait $PID1
+CUDA_VISIBLE_DEVICES=0,1 python main.py --config SEG/sup/random1_ODOC_sup_align5e-1 &
+CUDA_VISIBLE_DEVICES=2,3 python main.py --config SEG/sup/random1_ODOC_sup_align6e-1 &
+CUDA_VISIBLE_DEVICES=4,5 python main.py --config SEG/sup/random1_ODOC_sup_align7e-1 &
+PID2=$!
+
+wait $PID2
+CUDA_VISIBLE_DEVICES=0,1 python main.py --config SEG/sup/random1_ODOC_sup_align8e-1 &
+CUDA_VISIBLE_DEVICES=2,3 python main.py --config SEG/sup/random1_ODOC_sup_align9e-1 &
+CUDA_VISIBLE_DEVICES=4,5 python main.py --config SEG/sup/random1_ODOC_sup_align10e-1 &
+PID3=$!
+
 
 
