487d4f9d4b868d7dee822d766d5b00d8012df506
yzc <yzc@qq.com>
2023-07-28 19:45:19 +0800

    add

--- preprocess/compute_class_weight.py
+++ preprocess/compute_class_weight.py
@@ -2,23 +2,25 @@ import os
 from PIL import Image
 import numpy as np
 
-path = 'dataset/SEG/sup/random1/training.txt'
-
+root = '../data/fundus_datasets/od_oc/SEG/'
+path = '../dataset/SEG/sup/random1/training.txt'
 
 od_ratio = 0.0
 oc_ratio = 0.0
 count = 0
-with open(path,'r') as f:
+with open(path, 'r') as f:
     ids = f.read().splitlines()
     for i in ids:
         count += 0
         mask_path = i.split(' ')[-1]
-        mask_arr = np.array(Image.open(mask_path))
-        w,h = mask_arr.shape()
-        od_count = mask_arr.count(1)
-        oc_count = mask_arr.count(2)
-        od_ratio += od_count/(w*h)
-        oc_ratio += oc_count/(w*h)
+        mask_arr = np.array(Image.open(os.path.join(root, mask_path)))
 
-print('od_ratio:{}'.format(od_ratio))
-print('oc_ratio:{}'.format(oc_ratio))
+        counts = np.bincount(mask_arr.flatten())
+        now_od_ratio = counts[1] / sum(counts)
+        now_oc_ratio = counts[2] / sum(counts)
+
+        od_ratio += now_od_ratio
+        oc_ratio += now_oc_ratio
+
+print('od_ratio:{}'.format(od_ratio / count))
+print('oc_ratio:{}'.format(oc_ratio / count))
