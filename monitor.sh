42ecef4c1d55fde00fef493746fa5c5116c635b5
yzc <yzc@qq.com>
2023-07-30 00:48:43 +0800

    add

--- monitor.sh
+++ monitor.sh
@@ -2,7 +2,7 @@
 
 # Function to check GPU utilization
 check_gpu_utilization() {
-  gpu_utilization=$(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits -i 0,1,2,3,4,5| grep -o '[0-9]\+' | awk '{print $1}')
+  gpu_utilization=$(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits -i 0,1,2,3| grep -o '[0-9]\+' | awk '{print $1}')
   echo "$gpu_utilization"
 }
 
@@ -10,22 +10,7 @@ check_gpu_utilization() {
 execute_task() {
   # Replace this function with the logic to execute your task on the specified GPU
   echo "Executing your task on GPU"
-  CUDA_VISIBLE_DEVICES=0,1 python main.py --config SEG/semi/50/ODOC_semi50_align1e-1 &
-  CUDA_VISIBLE_DEVICES=2,3 python main.py --config SEG/semi/50/ODOC_semi50_align2e-1 &
-  CUDA_VISIBLE_DEVICES=4,5 python main.py --config SEG/semi/50/ODOC_semi50_align3e-1 &
-  PID4=$!
-
-  wait $PID4
-  CUDA_VISIBLE_DEVICES=0,1 python main.py --config SEG/semi/50/ODOC_semi50_align4e-1 &
-  CUDA_VISIBLE_DEVICES=2,3 python main.py --config SEG/semi/50/ODOC_semi50_align5e-1 &
-  CUDA_VISIBLE_DEVICES=4,5 python main.py --config SEG/semi/50/ODOC_semi50_align6e-1 &
-  PID5=$!
-
-  wait $PID5
-  CUDA_VISIBLE_DEVICES=0,1 python main.py --config SEG/semi/50/ODOC_semi50_align7e-1 &
-  CUDA_VISIBLE_DEVICES=2,3 python main.py --config SEG/semi/50/ODOC_semi50_align8e-1 &
-  CUDA_VISIBLE_DEVICES=4,5 python main.py --config SEG/semi/50/ODOC_semi50_align9e-1 &
-  PID6=$!
+  /usr/bin/shutdown
 }
 
 # Main loop
