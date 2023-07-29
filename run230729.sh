23fa5a0f6d2f72d907dd40142433f7971fab5b21
yzc <yzc@qq.com>
2023-07-29 18:07:55 +0800

    add

--- run230729.sh
+++ run230729.sh
@@ -6,4 +6,7 @@ CUDA_VISIBLE_DEVICES=4,5 python main.py --config SEG/semi/50/ODOC_semi50_align10
 #
 CUDA_VISIBLE_DEVICES=0,1 python main.py --config SEG/sup/Dual_random1_ODOC_sup
 
+#
+CUDA_VISIBLE_DEVICES=0,1 python main.py --config SEG/sup/random1_OD_sup &
+CUDA_VISIBLE_DEVICES=2,3 python main.py --config SEG/sup/random1_OC_sup
 
