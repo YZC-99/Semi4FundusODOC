ed01b2b9dee75b771390a945e5e729291d9c92c1
yzc <yzc@qq.com>
2023-07-30 00:47:41 +0800

    add

--- run230730.sh
+++ run230730.sh
@@ -1,4 +1,8 @@
 #
-CUDA_VISIBLE_DEVICES=0,1 python main.py --config SEG/sup/random1_ODOC_sup_align3e-1_logitsTransform
+CUDA_VISIBLE_DEVICES=0,1 python main.py --config SEG/sup/random1_ODOC_sup_align3e-1_logitsTransform &
+CUDA_VISIBLE_DEVICES=2,3 python main.py --config SEG/semi/50/ODOC_semi50_align3e-1_logitsTransform &
+PID1=$!
+wait $PID1
+/usr/bin/shutdown
 
 
