d349b48573052639ef420768e02320f6361f5267
yzc <yzc@qq.com>
2023-08-01 22:33:03 +0800

    add

--- segment/losses/blv_loss.py
+++ segment/losses/blv_loss.py
@@ -23,8 +23,14 @@ class BlvLoss(nn.Module):
         self.sampler = normal.Normal(0, sigma)
         self._loss_name = 'BlvLoss'
 
-    def forward(self, pred, target,viariation):
-        # viariation = self.sampler.sample(pred.shape).clamp(-1, 1)
+    def forward(self, pred, target):
+        viariation = self.sampler.sample(pred.shape).clamp(-1, 1)
+        viariation = viariation.to(pred.device)
+        self.frequency_list = self.frequency_list.to(pred.device)
+        print(viariation.device)
+        print(self.frequency_list.device)
+        print(pred.device)
+        print(target.device)
         pred = pred + (viariation.abs().permute(0, 2, 3, 1) / self.frequency_list.max() * self.frequency_list).permute(0, 3, 1, 2)
         loss = F.cross_entropy(pred, target, reduction='none')
         return loss
