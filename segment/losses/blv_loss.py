880ca8e6a75ceb49837c0c5705796bfc4c9ce042
yzc <yzc@qq.com>
2023-08-01 21:45:55 +0800

    add

--- segment/losses/blv_loss.py
+++ segment/losses/blv_loss.py
@@ -11,8 +11,7 @@ import functools
 import numpy as np
 import torch
 import torch.nn.functional as F
-import pytorch_lightning as pl
-class BlvLoss(pl.LightningModule):
+class BlvLoss(nn.Module):
 #cls_nufrequency_list
     def __init__(self, cls_num_list, sigma=4):
         super(BlvLoss, self).__init__()
@@ -24,9 +23,9 @@ class BlvLoss(pl.LightningModule):
         self.sampler = normal.Normal(0, sigma)
 
     def forward(self, pred, target):
-
-
-        viariation = self.sampler.sample(pred.shape).clamp(-1, 1).to(self.device)
+        viariation = self.sampler.sample(pred.shape).clamp(-1, 1).to(pred.device)
+        target = target.to(pred.device)
+        self.frequency_list = self.frequency_list.to(pred.device)
         pred = pred + (viariation.abs().permute(0, 2, 3, 1) / self.frequency_list.max() * self.frequency_list).permute(0, 3, 1, 2)
 
         loss = F.cross_entropy(pred, target, reduction='none')
