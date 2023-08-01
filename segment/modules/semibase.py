bb5050490b93bb96c0d5f90d5f6cc7d276da4252
yzc <yzc@qq.com>
2023-08-01 22:22:33 +0800

    add

--- segment/modules/semibase.py
+++ segment/modules/semibase.py
@@ -54,7 +54,7 @@ class Base(pl.LightningModule):
             self.sampler = normal.Normal(0, 4)
             cls_num_list = torch.tensor([200482,42736,18925])
             frequency_list = torch.log(cls_num_list)
-            self.frequency_list = torch.log(sum(cls_num_list)) - frequency_list
+            self.frequency_list = (torch.log(sum(cls_num_list)) - frequency_list).to(self.device)
 
         if cfg.MODEL.logitsTransform:
             self.confidence_layer = nn.Sequential(
@@ -104,7 +104,7 @@ class Base(pl.LightningModule):
             output_out = scores_out_tmp + (1 - confidence) * logits
             out['out'] = output_out
         if self.cfg.MODEL.BlvLoss:
-            viariation = self.sampler.sample(logits.shape).clamp(-1, 1)
+            viariation = self.sampler.sample(logits.shape).clamp(-1, 1).to(self.device)
             logits = logits + (viariation.abs().permute(0, 2, 3, 1) / self.frequency_list.max() * self.frequency_list).permute(0, 3, 1, 2)
             out['out'] = logits
         return out
