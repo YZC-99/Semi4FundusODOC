47a62732e089c5e142d0ba55cbb8a920465b4c7b
yzc <yzc@qq.com>
2023-08-01 17:17:42 +0800

    add

--- run230801.sh
+++ run230801.sh
@@ -1,4 +1,5 @@
 CUDA_VISIBLE_DEVICES=0,1 python main.py --config SEG/cropped_sup/random1_ODOC_sup_align3e-1_logitsTransform
+CUDA_VISIBLE_DEVICES=0,1 python main.py --config SEG/cropped_sup/random1_ODOC_sup_blv
 
 
 
