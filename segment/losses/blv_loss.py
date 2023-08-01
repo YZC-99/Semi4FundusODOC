1ae9ddfa83a8d1d440a67124be5070d45eb50777
yzc <yzc@qq.com>
2023-08-01 21:41:09 +0800

    add

--- segment/losses/blv_loss.py
+++ segment/losses/blv_loss.py
@@ -11,26 +11,22 @@ import functools
 import numpy as np
 import torch
 import torch.nn.functional as F
-
-class BlvLoss(nn.Module):
+import pytorch_lightning as pl
+class BlvLoss(pl.LightningModule):
 #cls_nufrequency_list
     def __init__(self, cls_num_list, sigma=4):
         super(BlvLoss, self).__init__()
-        cls_num_list = np.array(cls_num_list)
-        cls_list = cls_num_list.astype(np.float32)
-        frequency_list = np.log(cls_list)
-        self.frequency_list = np.log(sum(cls_num_list)) - frequency_list
+
+        cls_num_list = torch.tensor(cls_num_list)
+        cls_list = torch.tensor(cls_num_list, dtype=torch.float)
+        frequency_list = torch.log(cls_list)
+        self.frequency_list = torch.log(sum(cls_num_list)) - frequency_list
         self.sampler = normal.Normal(0, sigma)
 
-        # cls_num_list = torch.tensor(cls_num_list)
-        # cls_list = torch.tensor(cls_num_list, dtype=torch.float)
-        # frequency_list = torch.log(cls_list)
-        # self.frequency_list = torch.log(sum(cls_num_list)) - frequency_list
-        # self.sampler = normal.Normal(0, sigma)
+    def forward(self, pred, target,cls_num_list):
 
-    def forward(self, pred, target):
 
-        viariation = self.sampler.sample(pred.shape).clamp(-1, 1)
+        viariation = self.sampler.sample(pred.shape).clamp(-1, 1).to(self.device)
         pred = pred + (viariation.abs().permute(0, 2, 3, 1) / self.frequency_list.max() * self.frequency_list).permute(0, 3, 1, 2)
 
         loss = F.cross_entropy(pred, target, reduction='none')
