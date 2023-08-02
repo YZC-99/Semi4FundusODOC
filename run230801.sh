cd24851485ba5c10d80859e1c4384f92974c5de5
yzc <yzc@qq.com>
2023-08-02 17:25:34 +0800

    add

--- run230801.sh
+++ run230801.sh
@@ -10,4 +10,5 @@ wait $PID3
 #/usr/bin/shutdown
 
 CUDA_VISIBLE_DEVICES=0,1 python main.py --config SEG/cropped_sup/random1_ODOC_sup_align3e-1_logitsTransform-V2
+CUDA_VISIBLE_DEVICES=0,1 python main.py --config SEG/cropped_sup/random1_ODOC_sup_align3e-1_logitsTransform-V3
 
