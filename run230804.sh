bd7d0d3c0314fda29156bb34289389a6f5a7170e
yzc <yzc@qq.com>
2023-08-05 20:49:22 +0800

    add

--- run230804.sh
+++ run230804.sh
@@ -1,7 +1,6 @@
 CUDA_VISIBLE_DEVICES=0,1 python main.py --config SEG/cropped_sup512x512/res34deeplabv3plus/random1_ODOC_sup &
 CUDA_VISIBLE_DEVICES=0,1 python main.py --config SEG/cropped_sup512x512/res34deeplabv3plus/random1_ODOC_sup_align3e-1_logitsTransform &
 CUDA_VISIBLE_DEVICES=0,1 python main.py --config SEG/cropped_sup512x512/res34deeplabv3plus/random1_ODOC_sup_blvLoss &
-CUDA_VISIBLE_DEVICES=0,1 python main.py --config SEG/cropped_sup512x512/res34deeplabv3plus/random1_ODOC_sup_blvLoss &
 CUDA_VISIBLE_DEVICES=0,1 python main.py --config SEG/cropped_sup512x512/res34deeplabv3plus/random1_ODOC_sup90 &
 CUDA_VISIBLE_DEVICES=0,1 python main.py --config SEG/cropped_sup512x512/res34deeplabv3plus/random1_ODOC_sup80 &
 
