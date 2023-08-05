22e49072e8c5b649a26684561eee8397d9f6ece0
yzc <yzc@qq.com>
2023-08-05 20:05:02 +0800

    add

--- run230804.sh
+++ run230804.sh
@@ -1,7 +1,7 @@
-CUDA_VISIBLE_DEVICES=0,1 python main.py --config SEG/cropped_sup256x256/random1_ODOC_sup &
-CUDA_VISIBLE_DEVICES=0,1 python main.py --config SEG/cropped_sup256x256/random1_ODOC_sup_align3e-1_logitsTransform &
-CUDA_VISIBLE_DEVICES=0,1 python main.py --config SEG/cropped_sup256x256/random1_ODOC_sup_blvLoss &
-CUDA_VISIBLE_DEVICES=2,3 python main.py --config SEG/cropped_sup256x256/random1_ODOC_sup90 &
-CUDA_VISIBLE_DEVICES=2,3 python main.py --config SEG/cropped_sup256x256/random1_ODOC_sup80 &
-CUDA_VISIBLE_DEVICES=2,3 python main.py --config SEG/cropped_sup256x256/random1_ODOC_sup70 &
+CUDA_VISIBLE_DEVICES=0,1 python main.py --config SEG/cropped_sup512x512/random1_ODOC_sup &
+CUDA_VISIBLE_DEVICES=0,1 python main.py --config SEG/cropped_sup512x512/random1_ODOC_sup_align3e-1_logitsTransform &
+CUDA_VISIBLE_DEVICES=0,1 python main.py --config SEG/cropped_sup512x512/random1_ODOC_sup_blvLoss &
+CUDA_VISIBLE_DEVICES=2,3 python main.py --config SEG/cropped_sup512x512/random1_ODOC_sup90 &
+CUDA_VISIBLE_DEVICES=2,3 python main.py --config SEG/cropped_sup512x512/random1_ODOC_sup80 &
+CUDA_VISIBLE_DEVICES=2,3 python main.py --config SEG/cropped_sup512x512/random1_ODOC_sup70 &
 
