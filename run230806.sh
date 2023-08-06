860aaa116f1587e9b69e45360bf9217ba7121180
yzc <yzc@qq.com>
2023-08-06 16:33:37 +0800

    add

--- run230806.sh
+++ run230806.sh
@@ -2,7 +2,7 @@
 #CUDA_VISIBLE_DEVICES=0,1 python main.py --config SEG/cropped_semi512x512/res50deeplabv3plus/random1_ODOC_semi90 &
 #CUDA_VISIBLE_DEVICES=2,3 python main.py --config SEG/cropped_semi512x512/res50deeplabv3plus/random1_ODOC_semi90_scratch_tgt_prototype
 
-CUDA_VISIBLE_DEVICES=0 python main.py --num_gpus 1 --config refuge_select/cropped/all &
-CUDA_VISIBLE_DEVICES=1 python main.py --num_gpus 1 --config refuge_select/cropped/test &
-CUDA_VISIBLE_DEVICES=2 python main.py --num_gpus 1 --config refuge_select/cropped/train &
-CUDA_VISIBLE_DEVICES=3 python main.py --num_gpus 1 --config refuge_select/cropped/val &
+CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config refuge_select/cropped/all &
+CUDA_VISIBLE_DEVICES=1 python main.py -ng 1 --config refuge_select/cropped/test &
+CUDA_VISIBLE_DEVICES=2 python main.py -ng 1 --config refuge_select/cropped/train &
+CUDA_VISIBLE_DEVICES=3 python main.py -ng 1 --config refuge_select/cropped/val &
