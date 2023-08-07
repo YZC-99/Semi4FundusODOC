58be24c66c382bb3e2c9a26fdf969902fce9ade0
yzc <yzc@qq.com>
2023-08-07 12:35:12 +0800

    add

--- run230807.sh
+++ run230807.sh
@@ -1,5 +1,5 @@
 # done
 CUDA_VISIBLE_DEVICES=0,1 python main.py --config SEG/cropped_sup512x512/res50deeplabv3plus/random1_ODOC_sup_excludeBADv1
 CUDA_VISIBLE_DEVICES=0,1 python main.py --config SEG/cropped_sup512x512/res50deeplabv3plus/random1_ODOC_sup_excludeBADv2
-/usr/bin/shutdown
-
+#/usr/bin/shutdown
+CUDA_VISIBLE_DEVICES=2,3 python main.py --config SEG/cropped_semi512x512/res50deeplabv3plus/random1_ODOC_semi90_None
