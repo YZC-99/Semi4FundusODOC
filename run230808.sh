c541ce992bb8257bbdc107b5761706e3fdc4aec9
yzc <yzc@qq.com>
2023-08-08 20:39:03 +0800

    add

--- run230808.sh
+++ run230808.sh
@@ -1,2 +1,2 @@
 # done
-CUDA_VISIBLE_DEVICES=0,1 python main.py --config SEG/cropped_sup512x512/Teacher_Student/random1_ODOC_semi90
+CUDA_VISIBLE_DEVICES=0 python main.py --ng 1 --config SEG/cropped_sup512x512/Teacher_Student/random1_ODOC_semi90
