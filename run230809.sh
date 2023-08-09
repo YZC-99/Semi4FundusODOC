50c99753be63698cf10892a0ee423385eb72823b
yzc <yzc@qq.com>
2023-08-09 22:21:47 +0800

    add

--- run230809.sh
+++ run230809.sh
@@ -2,6 +2,8 @@
 #CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config SEG/cropped_semi512x512/Teacher_Student/random1_ODOC_semi90_None &
 #CUDA_VISIBLE_DEVICES=1 python main.py -ng 1 --config SEG/cropped_semi512x512/Teacher_Student/random1_ODOC_semi90_None_weightedLoss
 
+#CUDA_VISIBLE_DEVICES=1 python main.py -ng 1 --config SEG/cropped_sup512x512/random1_ODOC_sup10_DCBDLoss
+
 
 
 CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config SEG/cropped_semi512x512/Teacher_Student/random1_ODOC_semi90_PretrainT_PretrainS_None &
