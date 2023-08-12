5548ca7334542ce45fde5c966228844602b80069
yzc <yzc@qq.com>
2023-08-12 19:41:15 +0800

    add

--- refer.py
+++ refer.py
@@ -19,7 +19,7 @@ from torch.utils.data import DataLoader
 
 #
 num_classes = 3
-ckpt_path = '/root/autodl-tmp/Semi4FundusODOC/experiments/domain_shift_sup/random1_RERUGE400Drishti_sup/ckpt/val_OD_dice=0.932737.ckpt'
+ckpt_path = '/root/autodl-tmp/Semi4FundusODOC/experiments/SEG/cropped_semi512x512/res50deeplabv3plus/random1_ODOC_semi90_None_minus_Boundary2/ckpt/epoch=45-val_OD_dice=0.963002.ckpt'
 log_path = 'experiments/preds'
 model_zoo = {'deeplabv3plus': DeepLabV3Plus, 'pspnet': PSPNet, 'deeplabv2': DeepLabV2}
 model = model_zoo['deeplabv3plus']('resnet50', num_classes)
@@ -38,34 +38,34 @@ for key, value in sd.items():
     key = key.replace('model.', '')
     new_state_dict[key] = value
 model.load_state_dict(new_state_dict)
-model.to('cuda:3')
+model.to('cuda:0')
 model.eval()
 
 dataset = SupTrain(task='od_oc',
-                            name='REFUGE/cropped_sup/select_all',
-                            root='./data/fundus_datasets/od_oc/SEG/',
-                            mode='val',
-                            size=512
+                    name='SEG/cropped_semi/random1',
+                    root='./data/fundus_datasets/od_oc/SEG/',
+                    mode='test',
+                    size=512
                              )
 dataloader = DataLoader(dataset, batch_size=1, shuffle=False,
                                   pin_memory=True, num_workers=8, drop_last=False)
 tbar = tqdm(dataloader)
-od_mIoU = JaccardIndex(num_classes=2, task='multiclass').to('cuda:3')
-oc_mIoU = JaccardIndex(num_classes=2, task='multiclass').to('cuda:3')
-od_Dice = Dice(num_classes=1,multiclass=False).to('cuda:3')
-oc_Dice = Dice(num_classes=1,multiclass=False).to('cuda:3')
+od_mIoU = JaccardIndex(num_classes=2, task='multiclass').to('cuda:0')
+oc_mIoU = JaccardIndex(num_classes=2, task='multiclass').to('cuda:0')
+od_Dice = Dice(num_classes=1,multiclass=False).to('cuda:0')
+oc_Dice = Dice(num_classes=1,multiclass=False).to('cuda:0')
 cmap = color_map('eye')
 if not os.path.exists(log_path):
     os.mkdir(log_path)
 # 创建csv文件
-with open(os.path.join('experiments','preds_metrics.csv'), 'w', newline='') as file:
+with open(os.path.join('refers','preds_metrics.csv'), 'w', newline='') as file:
     writer = csv.writer(file)
     writer.writerow(['ID', 'OD_mIoU','OD_Dice','OC_mIoU','OC_Dice'])  # 写入表头
     with torch.no_grad():
         for batch in tbar:
             img,mask,id = batch['img'],batch['mask'],batch['id']
-            mask = mask.to('cuda:3')
-            img = img.to('cuda:3')
+            mask = mask.to('cuda:0')
+            img = img.to('cuda:0')
             logits = model(img)['out']
             preds = nn.functional.softmax(logits, dim=1).argmax(1)
 
@@ -86,17 +86,10 @@ with open(os.path.join('experiments','preds_metrics.csv'), 'w', newline='') as f
             od_cover_preds = od_preds + oc_preds
             od_cover_preds[od_cover_preds > 0] = 1
 
-            # od_mIoU(od_cover_gt,od_cover_preds)
-            # od_Dice(od_cover_gt,od_cover_preds)
+            # pred = Image.fromarray(preds.squeeze(0).cpu().detach().numpy().astype(np.uint8), mode='P')
+            # pred.putpalette(cmap)
             #
-            # oc_mIoU(oc_mask,oc_preds)
-            # oc_Dice(oc_mask,oc_preds)
-
-            pred = Image.fromarray(preds.squeeze(0).cpu().detach().numpy().astype(np.uint8), mode='P')
-            pred.putpalette(cmap)
-
-            pred.save('%s/%s' % (log_path, os.path.basename(id[0].split(' ')[1])))
-
+            # pred.save('%s/%s' % (log_path, os.path.basename(id[0].split(' ')[1])))
             # 写入csv
             writer.writerow([id[0],
                              round(od_mIoU(od_cover_gt,od_cover_preds).item()*100,2),
