cc81305ffaa792204d97da5222a5f34380a91a9f
yzc <yzc@qq.com>
2023-08-13 14:47:33 +0800

    add

--- run230813.sh
+++ run230813.sh
@@ -1,3 +1,4 @@
 CUDA_VISIBLE_DEVICES=0,1 python main.py  --config SEG/cropped_semi512x512/res50deeplabv3plus/random1_ODOC_semi10_None_scratch_DCBDLoss
+CUDA_VISIBLE_DEVICES=0,1 python main.py  --config SEG/cropped_semi512x512/res50deeplabv3plus/random1_ODOC_semi10_None_scratch_DCBDLoss
 
 # 我想git一下
