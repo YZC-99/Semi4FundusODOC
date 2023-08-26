2600e23eb9d8e6e077bd84e68cee1abf12f4992d
yzc <yzc@qq.com>
2023-08-26 20:54:09 +0800

    add

--- run230826.sh
+++ run230826.sh
@@ -1,3 +1,3 @@
-CUDA_VISIBLE_DEVICES=0,1 python main.py  --config SEG/cropped_sup512x512/random1_ODOC_sup10_CECBLLoss
-#CUDA_VISIBLE_DEVICES=0,1 python main.py  --config SEG/cropped_semi512x512/res50deeplabv3plus/random1_ODOC_semi10_scratch_CEIoUABLLoss_addARC705_G1020
+#CUDA_VISIBLE_DEVICES=0,1 python main.py  --config SEG/cropped_sup512x512/random1_ODOC_sup10_CECBLLoss
+CUDA_VISIBLE_DEVICES=0,1 python main.py  --config SEG/cropped_semi512x512/res50deeplabv3plus/random1_ODOC_semi10_scratch_CECBLLoss_addARC705_G1020
 
