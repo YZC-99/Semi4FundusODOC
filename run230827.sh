955a4accce99e92cc939935043c782c6d4e80b44
yzc <yzc@qq.com>
2023-08-27 12:33:15 +0800

    add

---
+++ run230827.sh
@@ -0,0 +1,3 @@
+CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config SEG/demo/random1_ODOC_sup10_CEIOUABLLoss
+CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config SEG/demo/random1_ODOC_sup10_CECBLLoss
+
