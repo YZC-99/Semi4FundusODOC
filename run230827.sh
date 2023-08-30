a77928e170e2d501d8faf875c12a18099f853d28
yzc <yzc@qq.com>
2023-08-30 16:41:50 +0800

    add

--- run230827.sh
+++ run230827.sh
@@ -2,4 +2,4 @@
 #CUDA_VISIBLE_DEVICES=0,1 python main.py  --config SEG/cropped_sup512x512/random1_ODOC_sup10_CEIOUABLLoss
 #CUDA_VISIBLE_DEVICES=0,1 python main.py  --config SEG/cropped_sup512x512/random1_ODOC_sup10_CECBLLoss
 #CUDA_VISIBLE_DEVICES=0,1 python main.py  --config SEG/cropped_sup512x512/random1_ODOC_sup10_CEPairwiseCBLLoss
-CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config SEG/cropped_sup512x512/random1_ODOC_sup10_CEContrastPixelCBLLoss
+CUDA_VISIBLE_DEVICES=0,1 python main.py --config SEG/cropped_sup512x512/random1_ODOC_sup10_CEContrastPixelCBLLoss
