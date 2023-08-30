6e72da29932a1cad5c36e6d7cc91f167329eded3
yzc <yzc@qq.com>
2023-08-30 16:09:20 +0800

    add

--- segment/modules/semibase.py
+++ segment/modules/semibase.py
@@ -12,7 +12,9 @@ from segment.losses.seg.boundary_loss import SurfaceLoss
 from segment.losses.seg.dice_loss import DiceLoss
 from segment.losses.seg.focal_loss import FocalLoss
 from segment.losses.abl import ABL
-from segment.losses.cbl import CBL,ContrastCenterCBL,CEpair_CBL,ContrastPixelCBL
+from segment.losses.cbl import CBL,ContrastCenterCBL,CEpair_CBL
+# from segment.losses.cbl import ContrastPixelCBL
+from segment.losses.cbl import ContrastPixelCBLV2 as ContrastPixelCBL
 from segment.losses.lovasz_loss import lovasz_softmax
 from segment.modules.prototype_dist_estimator import prototype_dist_estimator
 from typing import List,Tuple, Dict, Any, Optional
