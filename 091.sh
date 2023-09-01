adf54affe55326e4c30e8adde1046c958a6a2473
yzc <yzc@qq.com>
2023-09-01 13:34:03 +0800

    add

--- 091.sh
+++ 091.sh
@@ -4,4 +4,5 @@
 
 
 #CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -v 2 --config REFUGE/cropped_sup256x256/unet/random1_ODOC_DCBDFCLoss
-CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -v 2 --config Drishti-GS/cropped_sup256x256/unet/random1_ODOC_DCBDFCLoss
+CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config Drishti-GS/cropped_sup256x256/unet/random1_ODOC_DCBDFCLoss
+CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config Drishti-GS/cropped_sup256x256/res50deeplabv3plus/random1_ODOC_DCBDFCLoss
