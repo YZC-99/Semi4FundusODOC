ad518086a21d00160f5358c77fe3753976beda59
yzc <yzc@qq.com>
2023-09-01 22:22:02 +0800

    add

--- 091.sh
+++ 091.sh
@@ -1,8 +1,8 @@
-CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config REFUGE/cropped_sup512x512/random1_ODOC_DCBDFCLoss
-CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config REFUGE/cropped_sup512x512/random1_ODOC_CEIoUABLLoss
-CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config REFUGE/cropped_sup512x512/random1_ODOC_ContrastPixelCBLLoss
-CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config REFUGE/cropped_sup512x512/random1_ODOC_CrossContrastPixelCBLLoss
-CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config REFUGE/cropped_sup512x512/random1_ODOC_dySampleCEContrastCorrectPixelCBLLoss
+CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config REFUGE/cropped_sup512x512/res50deeplabv3plus/random1_ODOC_DCBDFCLoss
+CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config REFUGE/cropped_sup512x512/res50deeplabv3plus/random1_ODOC_CEIoUABLLoss
+CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config REFUGE/cropped_sup512x512/res50deeplabv3plus/random1_ODOC_ContrastPixelCBLLoss
+CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config REFUGE/cropped_sup512x512/res50deeplabv3plus/random1_ODOC_CrossContrastPixelCBLLoss
+CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config REFUGE/cropped_sup512x512/res50deeplabv3plus/random1_ODOC_dySampleCEContrastCorrectPixelCBLLoss
 
 
 #CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -v 2 --config REFUGE/cropped_sup256x256/unet/random1_ODOC_DCBDFCLoss
