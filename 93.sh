5443152e618f1b9186ad4d7d971af6510a20f8f5
yzc <yzc@qq.com>
2023-09-01 08:40:27 +0800

    add

--- 93.sh
+++ 93.sh
@@ -1,1 +1,4 @@
 CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config REFUGE/cropped_sup512x512/random1_ODOC_DCBDFCLoss
+CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config REFUGE/cropped_sup512x512/random1_ODOC_CEIoUABLLoss
+CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config REFUGE/cropped_sup512x512/random1_ODOC_ContrastPixelCBLLoss
+CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config REFUGE/cropped_sup512x512/random1_ODOC_dySampleCEContrastCorrectPixelCBLLoss
