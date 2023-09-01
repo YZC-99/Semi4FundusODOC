0573e18c34f9c3adf029d892c7fd7a20896434e4
yzc <yzc@qq.com>
2023-09-01 09:02:35 +0800

    add

--- 93.sh
+++ 93.sh
@@ -1,4 +1,6 @@
-CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config REFUGE/cropped_sup256x256/random1_ODOC_DCBDFCLoss
+CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config REFUGE/cropped_sup256x256/random1_ODOC_DCBDFCLoss &
 CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config REFUGE/cropped_sup256x256/random1_ODOC_CEIoUABLLoss
-CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config REFUGE/cropped_sup256x256/random1_ODOC_ContrastPixelCBLLoss
-CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config REFUGE/cropped_sup256x256/random1_ODOC_dySampleCEContrastCorrectPixelCBLLoss
+
+CUDA_VISIBLE_DEVICES=1 python main.py -ng 1 --config REFUGE/cropped_sup256x256/random1_ODOC_ContrastPixelCBLLoss &
+CUDA_VISIBLE_DEVICES=2 python main.py -ng 1 --config REFUGE/cropped_sup256x256/random1_ODOC_dySampleCEContrastCorrectPixelCBLLoss &
+CUDA_VISIBLE_DEVICES=3 python main.py -ng 1 --config REFUGE/cropped_sup256x256/random1_ODOC_CrossContrastPixelCBLLoss &
