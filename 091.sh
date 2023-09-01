8bf757cc4e3d28d93665e4996405006dbbfe2f8b
yzc <yzc@qq.com>
2023-09-01 22:19:55 +0800

    add

--- 091.sh
+++ 091.sh
@@ -1,10 +1,12 @@
-#CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config REFUGE/cropped_sup256x256/random1_ODOC_DCBDFCLoss
-#CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config REFUGE/cropped_sup256x256/random1_ODOC_CEIoUABLLoss
-#CUDA_VISIBLE_DEVICES=1 python main.py -ng 1 --config REFUGE/cropped_sup256x256/random1_ODOC_ContrastPixelCBLLoss
+CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config REFUGE/cropped_sup512x512/random1_ODOC_DCBDFCLoss
+CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config REFUGE/cropped_sup512x512/random1_ODOC_CEIoUABLLoss
+CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config REFUGE/cropped_sup512x512/random1_ODOC_ContrastPixelCBLLoss
+CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config REFUGE/cropped_sup512x512/random1_ODOC_CrossContrastPixelCBLLoss
+CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config REFUGE/cropped_sup512x512/random1_ODOC_dySampleCEContrastCorrectPixelCBLLoss
 
 
 #CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -v 2 --config REFUGE/cropped_sup256x256/unet/random1_ODOC_DCBDFCLoss
 #CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config Drishti-GS/cropped_sup256x256/unet/random1_ODOC_DCBDFCLoss
 #CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config Drishti-GS/cropped_sup256x256/res50deeplabv3plus/random1_ODOC_DCBDFCLoss
-CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config Drishti-GS/cropped_sup256x256/res50deeplabv3plus/random1_ODOC_DCLoss
-CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config Drishti-GS/cropped_sup512x512/res50deeplabv3plus/random1_ODOC_DCLoss
+#CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config Drishti-GS/cropped_sup256x256/res50deeplabv3plus/random1_ODOC_DCLoss
+#CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config Drishti-GS/cropped_sup512x512/res50deeplabv3plus/random1_ODOC_DCLoss
