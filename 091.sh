1185850225ad118a261edc61eb8b5eff01cb778d
yzc <yzc@qq.com>
2023-09-02 18:10:15 +0800

    add

--- 091.sh
+++ 091.sh
@@ -1,4 +1,5 @@
 CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config Drishti-GS/cropped_sup512x512/unet/random1_ODOC_DCBDFCCrossContrastPixelCBLLoss &
+CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config Drishti-GS/cropped_sup512x512/unet/random1_ODOC_DCBDFCLoss
 
 
 
