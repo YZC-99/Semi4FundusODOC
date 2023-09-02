6a79c238c6b1aa8b5a6e667bbe8659d43ef92631
yzc <yzc@qq.com>
2023-09-02 22:54:56 +0800

    add

--- 091.sh
+++ 091.sh
@@ -1,27 +1,22 @@
-CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config Drishti-GS/cropped_sup512x512/unet/random1_ODOC_DCBDFCCrossContrastPixelCBLLoss
-CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config Drishti-GS/cropped_sup512x512/unet/random1_ODOC_lrdiff_DCBDFCLoss
-
-
-
 # 20230901
-#CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config REFUGE/cropped_sup512x512/res50deeplabv3plus/random1_ODOC_DCBDFCLoss &
-#CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config REFUGE/cropped_sup512x512/res50deeplabv3plus/random1_ODOC_CEIoUABLLoss &
-#CUDA_VISIBLE_DEVICES=1 python main.py -ng 1 --config Drishti-GS/cropped_sup512x512/res50deeplabv3plus/random1_ODOC_DCBDFCLoss &
-#CUDA_VISIBLE_DEVICES=1 python main.py -ng 1 --config Drishti-GS/cropped_sup512x512/res50deeplabv3plus/random1_ODOC_CEIoUABLLoss &
-#PID1=$!
-#wait $PID1
-#CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config REFUGE/cropped_sup512x512/res50deeplabv3plus/random1_ODOC_ContrastPixelCBLLoss &
-#CUDA_VISIBLE_DEVICES=1 python main.py -ng 1 --config Drishti-GS/cropped_sup512x512/res50deeplabv3plus/random1_ODOC_ContrastPixelCBLLoss &
-#PID2=$!
-#wait $PID2
-#CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config REFUGE/cropped_sup512x512/res50deeplabv3plus/random1_ODOC_dySampleCEContrastCorrectPixelCBLLoss &
-#CUDA_VISIBLE_DEVICES=1 python main.py -ng 1 --config Drishti-GS/cropped_sup512x512/res50deeplabv3plus/random1_ODOC_dySampleCEContrastCorrectPixelCBLLoss &
-#PID3=$!
-#wait $PID3
-#CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config REFUGE/cropped_sup512x512/res50deeplabv3plus/random1_ODOC_CrossContrastPixelCBLLoss &
-#CUDA_VISIBLE_DEVICES=1 python main.py -ng 1 --config Drishti-GS/cropped_sup512x512/res50deeplabv3plus/random1_ODOC_CrossContrastPixelCBLLoss &
-#PID4=$!
-#wait $PID4
+CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config REFUGE/cropped_sup512x512/res50deeplabv3plus/random1_ODOC_DCBDFCLoss &
+CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config REFUGE/cropped_sup512x512/res50deeplabv3plus/random1_ODOC_CEIoUABLLoss &
+CUDA_VISIBLE_DEVICES=1 python main.py -ng 1 --config Drishti-GS/cropped_sup512x512/res50deeplabv3plus/random1_ODOC_DCBDFCLoss &
+CUDA_VISIBLE_DEVICES=1 python main.py -ng 1 --config Drishti-GS/cropped_sup512x512/res50deeplabv3plus/random1_ODOC_CEIoUABLLoss &
+PID1=$!
+wait $PID1
+CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config REFUGE/cropped_sup512x512/res50deeplabv3plus/random1_ODOC_ContrastPixelCBLLoss &
+CUDA_VISIBLE_DEVICES=1 python main.py -ng 1 --config Drishti-GS/cropped_sup512x512/res50deeplabv3plus/random1_ODOC_ContrastPixelCBLLoss &
+PID2=$!
+wait $PID2
+CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config REFUGE/cropped_sup512x512/res50deeplabv3plus/random1_ODOC_dySampleCEContrastCorrectPixelCBLLoss &
+CUDA_VISIBLE_DEVICES=1 python main.py -ng 1 --config Drishti-GS/cropped_sup512x512/res50deeplabv3plus/random1_ODOC_dySampleCEContrastCorrectPixelCBLLoss &
+PID3=$!
+wait $PID3
+CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config REFUGE/cropped_sup512x512/res50deeplabv3plus/random1_ODOC_CrossContrastPixelCBLLoss &
+CUDA_VISIBLE_DEVICES=1 python main.py -ng 1 --config Drishti-GS/cropped_sup512x512/res50deeplabv3plus/random1_ODOC_CrossContrastPixelCBLLoss &
+PID4=$!
+wait $PID4
 #
 #
 ##----------------
