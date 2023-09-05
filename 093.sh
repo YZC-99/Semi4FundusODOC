60c438fd5f96eaed99df413a316f3867d5c5e497
yzc <yzc@qq.com>
2023-09-05 13:57:34 +0800

    add

--- 093.sh
+++ 093.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config SEG/cropped_semi512x512/res50deeplabv3plus/random1_ODOC_backbone_pretrained_Criss_Crossattention_flip_rotateDCBDFCLoss
+CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config Drishti-GS/cropped_sup512x512/res50deeplabv3plus/random1_ODOC_backbone_pretrained_Criss_Crossattention_flip_rotateDCBDFCLoss
 
 
 
