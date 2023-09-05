d3d590aea5999521f4c3c989419f582448ca3278
yzc <yzc@qq.com>
2023-09-05 12:20:13 +0800

    add

--- 091.sh
+++ 091.sh
@@ -5,7 +5,7 @@ CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config Drishti-GS/cropped_sup512x5
 CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config Drishti-GS/cropped_sup512x512/res50deeplabv3plus/random1_ODOC_backbone_pretrained_flip_DCBDFCrotateCrossContrastpixelCBLLossV5
 CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config Drishti-GS/cropped_sup512x512/res50deeplabv3plus/random1_ODOC_backbone_pretrained_flip_DCBDFCrotateCrossContrastpixelCBLLossV6
 CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config SEG/cropped_sup512x512/res50deeplabv3plus/random1_ODOC_sup10_backbone_pretrained_DCBDFCLoss
-CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config SEG/cropped_semi512x512/res50deeplabv3plus/random1_ODOC_semi10_backbone_pretrained_DCBDLoss
+CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config SEG/cropped_semi512x512/res50deeplabv3plus/random1_ODOC_semi10_backbone_pretrained_DCBDFCLoss
 
 #20230904
 CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config Drishti-GS/cropped_sup512x512/res50deeplabv3plus/random1_ODOC_backbone_pretrained_flip_rotateDCBDFCLoss
