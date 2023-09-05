5a7822763b39a8f9cda84b12c12a7bd324b62183
yzc <yzc@qq.com>
2023-09-05 19:16:12 +0800

    add

--- segment/modules/semseg/deeplabv3plus.py
+++ segment/modules/semseg/deeplabv3plus.py
@@ -274,7 +274,7 @@ class DeepLabV3Plus(BaseNet):
             c3 = self.criss_cross_attention1(c3)
             out_fuse_shape = out_fuse.shape[-2:]
             # 将out_fuse缩小
-            out_fuse = F.interpolate(out_fuse,size=out_fuse.shape[-2:], mode="bilinear", align_corners=True)
+            out_fuse = F.interpolate(out_fuse,size=c3.shape[-2:], mode="bilinear", align_corners=True)
             out_fuse_reducted = self.fuse_reduc(out_fuse)
             out_cross_criss_att = self.criss_cross_attention2.cross_forward(c3,out_fuse_reducted)
             out_fuse = F.interpolate(out_cross_criss_att,size=out_fuse_shape, mode="bilinear", align_corners=True)
