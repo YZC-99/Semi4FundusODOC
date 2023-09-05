6370dbf093e6410be959ff8fbc23c9e7dd286064
yzc <yzc@qq.com>
2023-09-05 15:33:07 +0800

    add

--- 093.sh
+++ 093.sh
@@ -1,6 +1,7 @@
 CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config Drishti-GS/cropped_sup512x512/res50deeplabv3plus/random1_ODOC_backbone_pretrained_Criss_Crossattention_flip_rotateDCBDFCLoss
 CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config Drishti-GS/cropped_sup512x512/res50deeplabv3plus/random1_ODOC_backbone_pretrained_Criss_CrossattentionR2_flip_rotateDCBDFCLoss
 CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config Drishti-GS/cropped_sup512x512/res50deeplabv3plus/random1_ODOC_backbone_pretrained_Criss_CrossattentionR2_V1_flip_rotateDCBDFCLoss
+CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config Drishti-GS/cropped_sup512x512/res50deeplabv3plus/random1_ODOC_backbone_pretrained_Criss_CrossattentionR2_V2_flip_rotateDCBDFCLoss
 
 
 
