0893832753e8bffd8d13fe0132202cae68bd85da
yzc <yzc@qq.com>
2023-09-06 15:54:45 +0800

    add

--- refer.py
+++ refer.py
@@ -43,7 +43,7 @@ model.eval()
 
 dataset = SupTrain(task='od_oc',
                     name='Drishti-GS/cropped_sup',
-                    root='/data/fundus_datasets/od_oc/Drishti-GS/',
+                    root='./data/fundus_datasets/od_oc/Drishti-GS/',
                     mode='test',
                     size=512
                              )
