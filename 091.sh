dac4fc70a40a9f1b1866b07f69654cad088b7fe5
yzc <yzc@qq.com>
2023-09-08 10:23:32 +0800

    add

--- 091.sh
+++ 091.sh
@@ -1,1 +1,12 @@
 CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -s 42 --config Drishti-GS/cropped_sup256x256/res50deeplabv3plus/backbone_pretrained_flip_rotate_scale_translateCEDCBDFCLoss
+
+CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -s 42 --config Drishti-GS/cropped_sup256x256/res50deeplabv3plus/aux1e_backbone_pretrained_flip_rotate_scale_translateCEDCBDFCLoss
+
+CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -s 42 --config Drishti-GS/cropped_sup256x256/res50deeplabv3plus/backbone_pretrained_flip_rotate_scale_translateCECBLLoss
+
+CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -s 42 --config Drishti-GS/cropped_sup256x256/res50deeplabv3plus/backbone_pretrained_flip_rotate_scale_translate_CEContrastPixelCBLLoss
+
+CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -s 42 --config Drishti-GS/cropped_sup256x256/res50deeplabv3plus/backbone_pretrained_flip_rotate_scale_translate_CEContrastCorrectPixelCBLLoss
+
+CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -s 42 --config Drishti-GS/cropped_sup256x256/res50deeplabv3plus/backbone_pretrained_flip_rotate_scale_translateCEDCBDFCLoss_Criss_Attention_R2_V1
+
