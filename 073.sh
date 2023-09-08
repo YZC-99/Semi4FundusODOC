f6b577b0fbf1e989305c05cae792fb28c27ecb04
yzc <yzc@qq.com>
2023-09-08 11:45:24 +0800

    add

--- 073.sh
+++ 073.sh
@@ -1,11 +1,5 @@
-CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config Drishti-GS/cropped_sup512x512/res50Mydeeplabv3plusplus/random1_ODOC_backbone_pretrained_flip_rotateDCBDFCLoss
-# 20230908
-CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config Drishti-GS/cropped_sup512x512/segformer/backbone_b2_pretrained_whole_inVOC_flip_rotateDCBDFCLoss_segheadlast
-CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config Drishti-GS/cropped_sup512x512/segformer/backbone_b2_pretrained_whole_inVOC_flip_rotateDCBDFCLoss
-CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config Drishti-GS/cropped_sup256x256/segformer/backbone_b2_pretrained_whole_inVOC_flip_rotateDCBDFCLoss
-CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config Drishti-GS/cropped_sup256x256/segformer/backbone_b2_pretrained_whole_inVOC_flip_rotateDCBDFCContrastCorrectPixelCBLLoss
-CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 --config Drishti-GS/cropped_sup256x256/segformer/backbone_b2_pretrained_whole_inVOC_flip_rotateDCBDFCLoss_segheadlast
+CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -s 42 --config Drishti-GS/cropped_sup256x256/segformer/backbone_b2_pretrained_flip_rotate_scale_translateDCBDFCLoss
 
+CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -s 42 --config Drishti-GS/cropped_sup256x256/segformer/backbone_b2_pretrained_whole_inVOC_flip_rotate_scale_translateDCBDFCLoss
 
-
-
+CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -s 42 --config Drishti-GS/cropped_sup256x256/segformer/backbone_b2_scratch_flip_rotate_scale_translateDCBDFCLoss
