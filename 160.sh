dcb9c7f8b83c8bf6d62a272eb39fd393484fa10a
yzc <yzc@qq.com>
2023-09-08 19:15:14 +0800

    add

--- 160.sh
+++ 160.sh
@@ -10,3 +10,4 @@ CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -s 42 -tune -alf --config Drishti-GS
 CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -s 42 -tune -alf --config Drishti-GS/cropped_sup256x256/segformer/backbone_b2_scratch_flip_rotate_scale_translateDCBDFCLoss
 
 CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -s 42 -tune -alf --config Drishti-GS/cropped_sup256x256/segformer/backbone_b4_pretrained_flip_rotate_scale_translateDCBDFCLoss
+CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -s 42 -tune -alf --config Drishti-GS/cropped_sup256x256/segformer/backbone_b5_pretrained_flip_rotate_scale_translateDCBDFCLoss
