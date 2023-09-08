8c114b43531fc65e4a6bef0ef999baa0c5870d80
yzc <yzc@qq.com>
2023-09-08 11:57:45 +0800

    add

--- 073.sh
+++ 073.sh
@@ -1,5 +1,4 @@
+#20230908
 CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -s 42 --config Drishti-GS/cropped_sup256x256/segformer/backbone_b2_pretrained_flip_rotate_scale_translateDCBDFCLoss
-
 CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -s 42 --config Drishti-GS/cropped_sup256x256/segformer/backbone_b2_pretrained_whole_inVOC_flip_rotate_scale_translateDCBDFCLoss
-
 CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -s 42 --config Drishti-GS/cropped_sup256x256/segformer/backbone_b2_scratch_flip_rotate_scale_translateDCBDFCLoss
--- 091.sh
+++ 091.sh
@@ -1,11 +1,13 @@
+#20230908
 CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -s 42 --config Drishti-GS/cropped_sup256x256/res50deeplabv3plus/backbone_pretrained_flip_rotate_scale_translateCEDCBDFCLoss
 CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -s 42 --config Drishti-GS/cropped_sup256x256/res50deeplabv3plus/aux1e_backbone_pretrained_flip_rotate_scale_translateCEDCBDFCLoss
 CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -s 42 --config Drishti-GS/cropped_sup256x256/res50deeplabv3plus/backbone_pretrained_flip_rotate_scale_translateCECBLLoss
 CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -s 42 --config Drishti-GS/cropped_sup256x256/res50deeplabv3plus/backbone_pretrained_flip_rotate_scale_translate_CEContrastPixelCBLLoss
 CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -s 42 --config Drishti-GS/cropped_sup256x256/res50deeplabv3plus/backbone_pretrained_flip_rotate_scale_translate_CEContrastCorrectPixelCBLLoss
 CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -s 42 --config Drishti-GS/cropped_sup256x256/res50deeplabv3plus/backbone_pretrained_flip_rotate_scale_translateCEDCBDFCLoss_Criss_Attention_R2_V1
-
 CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -s 42 --config Drishti-GS/cropped_sup256x256/res50deeplabv3plus/backbone_pretrained_flip_rotate_scale_translateCELoss
+
+CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -s 42 --config Drishti-GS/cropped_sup256x256/res50deeplabv3plus/backbone_pretrained_flip_rotate_scale_translate_CEIoUABLLoss
 
 
 
