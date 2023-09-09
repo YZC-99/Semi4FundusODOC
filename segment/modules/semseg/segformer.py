b6e387b9e87532e04468ec691cf0f657c4ed7c5c
yzc <yzc@qq.com>
2023-09-09 13:04:36 +0800

    add

--- segment/modules/semseg/segformer.py
+++ segment/modules/semseg/segformer.py
@@ -75,6 +75,18 @@ class SegFormerHead(nn.Module):
                 c2=embedding_dim,
                 k=1,
             )
+        if self.attention == 'subv1-2':
+            self.criss_cross_attention_sub1 = CrissCrossAttention(embedding_dim)
+            self.criss_cross_attention_sub2 = CrissCrossAttention(embedding_dim)
+            self.cca_sub1 = CrissCrossAttention(embedding_dim)
+            self.cca_sub2 = CrissCrossAttention(embedding_dim)
+            self.cca_sub1_sub2 = CrissCrossAttention(embedding_dim)
+
+            self.linear_fuse = ConvModule(
+                c1=embedding_dim*5,
+                c2=embedding_dim,
+                k=1,
+            )
         elif self.attention == 'subv2':
             # 我自己加的
             self.criss_cross_attention_sub1 = CrissCrossAttention(embedding_dim)
@@ -165,7 +177,7 @@ class SegFormerHead(nn.Module):
             sub2_att = self.criss_cross_attention_sub2(sub2)
             sub1_cross_sub2_att = self.criss_cross_attention_sub1_cross_sub2.cross_forward(sub1_att,sub2_att)
             _c = self.linear_fuse(torch.cat([sub1_cross_sub2_att,_c4, _c3, _c2, _c1], dim=1))
-        if self.attention == 'subv1-1':
+        elif self.attention == 'subv1-1':
             sub1 = _c1 - _c2
             sub2 = _c3 - _c4
             sub1_att = self.criss_cross_attention_sub1(sub1)
@@ -173,6 +185,15 @@ class SegFormerHead(nn.Module):
             sub1_cross_sub2_att_R1 = self.criss_cross_attention_sub1_cross_sub2_R1.cross_forward(sub1_att,sub2_att)
             sub1_cross_sub2_att_R2 = self.criss_cross_attention_sub1_cross_sub2_R2(sub1_cross_sub2_att_R1)
             _c = self.linear_fuse(torch.cat([sub1_cross_sub2_att_R2,_c4, _c3, _c2, _c1], dim=1))
+        elif self.attention == 'subv1-2':
+            sub1 = _c1 - _c2
+            sub2 = _c3 - _c4
+            sub1_att = self.criss_cross_attention_sub1(sub1)
+            sub2_att = self.criss_cross_attention_sub2(sub2)
+            sub1_att_cca = self.cca_sub1(sub1_att)
+            sub2_att_cca = self.cca_sub2(sub2_att)
+            sub1_sub2_att_cca = self.cca_sub1_sub2(sub1_att_cca,sub2_att_cca)
+            _c = self.linear_fuse(torch.cat([sub1_sub2_att_cca, _c4, _c3, _c2, _c1], dim=1))
         elif self.attention == 'subv2':
             sub1 = _c1 - _c2
             sub2 = _c3 - _c4
