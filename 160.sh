77da46f94cc9b4cc32ffe064b259548ad5e89274
yzc <yzc@qq.com>
2023-09-09 22:35:29 +0800

    add

--- 160.sh
+++ 160.sh
@@ -8,8 +8,9 @@ CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -s 42 --config Drishti-GS/cropped_su
 CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -s 42 --config Drishti-GS/cropped_sup256x256/segformer/backbone_b2_pretrained_flip_rotate_scale_translate_noise_cutoutDCBDFCLoss_attention_subv1-2
 CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -s 42 --config Drishti-GS/cropped_sup256x256/segformer/backbone_b2_pretrained_flip_rotate_scale_translate_noise_cutoutDCBDFCLoss_attention_sub_addv1
 CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -s 42 --config Drishti-GS/cropped_sup256x256/segformer/backbone_b2_pretrained_flip_rotate_scale_translate_noise_cutoutDCBDFCLoss_attention_sub_addv2
-
 CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -s 42 --config Drishti-GS/cropped_sup256x256/segformer/backbone_b2_pretrained_flip_rotate_scale_translate_noise_cutoutDCBDFCLoss_attention_sub_addv3
+
+CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -s 42 --config Drishti-GS/cropped_sup256x256/segformer/backbone_b2_pretrained_flip_rotate_scale_translate_noise_cutoutDCBDFCLoss_attention_backbone_subv1
 
 
 
--- segment/modules/semseg/segformer.py
+++ segment/modules/semseg/segformer.py
@@ -169,6 +169,19 @@ class SegFormerHead(nn.Module):
                 c2=embedding_dim,
                 k=1,
             )
+        elif attention == 'backbone_subv1':
+            self.lateral_c1 = ConvModule(c1_in_channels,768)
+            self.lateral_c2 = ConvModule(c2_in_channels,768)
+            self.lateral_c3 = ConvModule(c3_in_channels,768)
+            self.lateral_c4 = ConvModule(c4_in_channels,768)
+            self.cca1 = CrissCrossAttention(embedding_dim)
+            self.cca2 = CrissCrossAttention(embedding_dim)
+            self.linear_sub_fuse = MLP(input_dim=embedding_dim * 2, embed_dim=embedding_dim)
+            self.linear_fuse = ConvModule(
+                c1=embedding_dim*5,
+                c2=embedding_dim,
+                k=1,
+            )
 
         else:
             self.linear_fuse = ConvModule(
@@ -280,6 +293,27 @@ class SegFormerHead(nn.Module):
             _sub = self.linear_sub_fuse(torch.cat([sub1,sub2],dim=1))
             _sub = self.linear_sub_fuse_cca(_sub)
             _c = self.linear_fuse(torch.cat([_c4, _c3, _c2, _c1, _sub], dim=1))
+        elif self.attention == 'backbone_subv1':
+            # 先统一通道
+            lateral_c1 = self.lateral_c1(c1)
+            lateral_c2 = self.lateral_c2(c2)
+            lateral_c3 = self.lateral_c3(c3)
+            lateral_c4 = self.lateral_c4(c4)
+            # 全部上采样到128*128
+            lateral_c2 = F.interpolate(lateral_c2, size=lateral_c1.size()[2:], mode='bilinear', align_corners=False)
+            lateral_c3 = F.interpolate(lateral_c3, size=lateral_c1.size()[2:], mode='bilinear', align_corners=False)
+            lateral_c4 = F.interpolate(lateral_c4, size=lateral_c1.size()[2:], mode='bilinear', align_corners=False)
+            # 做相减
+            lateral_sub1 = lateral_c1 - lateral_c2
+            lateral_sub2 = lateral_c3 - lateral_c4
+            mlp_sub1 = _c1 - _c2
+            mlp_sub2 = _c3 - _c4
+            # 做crisscross attention
+            cca_result1 = self.cca1.cross_forward(lateral_sub1,mlp_sub1)
+            cca_result2 = self.cca1.cross_forward(lateral_sub2,mlp_sub2)
+            result = self.linear_sub_fuse(torch.cat([cca_result1,cca_result2],dim=1))
+            result = result.permute(0,2,1).reshape(n, -1, _c1.shape[2], _c1.shape[3])
+            _c = self.linear_fuse(torch.cat([_c4, _c3, _c2, _c1,result], dim=1))
         else:
             _c = self.linear_fuse(torch.cat([_c4, _c3, _c2, _c1], dim=1))
 
@@ -384,7 +418,7 @@ if __name__ == '__main__':
     # ckpt_path = '../../../pretrained/segformer_b2_weights_voc.pth'
     # sd = torch.load(ckpt_path,map_location='cpu')
 
-    model = SegFormer(num_classes=3, phi='b2', pretrained=False,attention='subv1')
+    model = SegFormer(num_classes=3, phi='b2', pretrained=False,attention='backbone_subv1')
     img = torch.randn(2,3,512,512)
     out = model(img)
     logits = out['out']
