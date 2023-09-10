583cb4e8ddbd2fad86298119c8561e3eee2f9a9c
yzc <yzc@qq.com>
2023-09-10 14:56:43 +0800

    add

--- segment/losses/ce_loss.py
+++ segment/losses/ce_loss.py
@@ -12,6 +12,8 @@ from torch.nn import functional as F
 class CrossEntropy(nn.Module):
     def __init__(self, ignore_label=-1, weight=None):
         super(CrossEntropy, self).__init__()
+        if not isinstance(weight, torch.Tensor):
+            weight = torch.tensor(weight)
         self.ignore_label = ignore_label
         self.criterion = nn.CrossEntropyLoss(weight=weight,
                                              ignore_index=ignore_label)
