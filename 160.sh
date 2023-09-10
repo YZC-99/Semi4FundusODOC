7e8729e6363509706ad256c62d86a743cf70be67
yzc <yzc@qq.com>
2023-09-10 14:53:49 +0800

    add

--- 160.sh
+++ 160.sh
@@ -26,6 +26,8 @@ CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -s 42 --config Drishti-GS/cropped_su
 
 CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -s 42 --config Drishti-GS/cropped_sup256x256/segformer/OHEM_backbone_b2_pretrained_flip_rotate_scale_translate_noise_cutoutDCBD_FC1e-1Loss_attention_sub_addv3
 
+CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -s 42 --config Drishti-GS/cropped_sup256x256/segformer/WeightCE_backbone_b2_pretrained_flip_rotate_scale_translate_noise_cutoutDCBD_FC1e-1Loss_attention_sub_addv3
+
 
 
 
