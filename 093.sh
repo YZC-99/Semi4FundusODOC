61df32a2480056a0e81249e1b364de5e26cf6343
yzc <yzc@qq.com>
2023-09-12 20:12:30 +0800

    add

--- 093.sh
+++ 093.sh
@@ -3,7 +3,8 @@ CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -s 42  --config Drishti-GS/cropped_s
 CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -s 42  --config Drishti-GS/cropped_sup256x256/segformerV2/OHEM9e-1_backbone_b2_pretrained_flip_rotate_scale_translate_noise_cutoutCE_3DC_3IoU_Loss_attention_sub_addv3
 CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -s 42  --config Drishti-GS/cropped_sup256x256/segformerV2/OHEM9e-1_backbone_b2_pretrained_flip_rotate_scale_translate_noise_cutoutCE_Contrast_Correct_Pixel_CBL_Loss_attention_sub_addv3
 CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -s 42  --config Drishti-GS/cropped_sup256x256/segformerV2/OHEM9e-1_backbone_b2_pretrained_flip_rotate_scale_translate_noise_cutoutCE_epoch10_Contrast_Correct_Pixel_CBL_Loss_attention_sub_addv3
-
 CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -s 42  --config Drishti-GS/cropped_sup256x256/segformerV2/OHEM9e-1_backbone_b2_pretrained_flip_rotate_scale_translate_noise_cutoutCE_DC_BD_Contrast_Correct_Pixel_CBL_Loss_attention_sub_addv3
+
+CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -s 42  --config Drishti-GS/cropped_sup256x256/segformerV2/OHEM9e-1_backbone_b2_pretrained_flip_rotate_scale_translate_noise_cutoutCE_IoU_Contrast_Correct_Pixel_CBL_Loss_attention_sub_addv3
 
 
