1c3410ce1dd9180e38b2a12563ed0a7cfea61c4e
yzc <yzc@qq.com>
2023-09-13 21:20:14 +0800

    add

--- segment/modules/semseg/segformer.py
+++ segment/modules/semseg/segformer.py
@@ -271,10 +271,9 @@ class SegFormerHead(nn.Module):
                 k=1,
             )
         elif attention == 'add_addv1':
-            self.sub_cca1 = CrissCrossAttention(embedding_dim*3)
-            self.sub_cca2 = CrissCrossAttention(embedding_dim*2)
+            self.sub_cca = CrissCrossAttention(embedding_dim*4)
             self.cca_fuse = ConvModule(
-                c1=embedding_dim * 6,
+                c1=embedding_dim * 4,
                 c2=embedding_dim,
                 k=1,
             )
@@ -502,16 +501,8 @@ class SegFormerHead(nn.Module):
             cca_fuse = self.cca_fuse(torch.cat([sub_cca1, sub_cca2, sub6], dim=1))
             _c = self.linear_fuse(torch.cat([_c4, _c3, _c2, _c1, cca_fuse], dim=1))
         elif self.attention == 'add_addv1':
-            sub1 = _c1 + _c2
-            sub2 = _c1 + _c3
-            sub3 = _c1 + _c4
-            sub4 = _c2 + _c3
-            sub5 = _c2 + _c4
-            sub6 = _c3 + _c4
-
-            sub_cca1 = self.sub_cca1(torch.cat([sub1, sub2, sub3], dim=1))
-            sub_cca2 = self.sub_cca2(torch.cat([sub4, sub5], dim=1))
-            cca_fuse = self.cca_fuse(torch.cat([sub_cca1, sub_cca2, sub6], dim=1))
+            sub_cca = self.sub_cca(torch.cat([_c1,_c2,_c3,_c4], dim=1))
+            cca_fuse = self.cca_fuse(sub_cca)
             _c = self.linear_fuse(torch.cat([_c4, _c3, _c2, _c1, cca_fuse], dim=1))
         elif self.attention == 'sub_or_multi_addv1':
             p = 0.5
