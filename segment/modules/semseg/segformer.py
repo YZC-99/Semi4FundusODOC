5a7c94358870e260b76167ced017a32a1b48df5a
yzc <yzc@qq.com>
2023-09-13 20:55:09 +0800

    add

--- segment/modules/semseg/segformer.py
+++ segment/modules/semseg/segformer.py
@@ -3,6 +3,8 @@
 #
 # This work is licensed under the NVIDIA Source Code License
 # ---------------------------------------------------------------
+import random
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -268,6 +270,19 @@ class SegFormerHead(nn.Module):
                 c2=embedding_dim,
                 k=1,
             )
+        elif attention == 'sub_or_multi_addv1':
+            self.sub_cca1 = CrissCrossAttention(embedding_dim*3)
+            self.sub_cca2 = CrissCrossAttention(embedding_dim*2)
+            self.cca_fuse = ConvModule(
+                c1=embedding_dim * 6,
+                c2=embedding_dim,
+                k=1,
+            )
+            self.linear_fuse = ConvModule(
+                c1=embedding_dim * 5,
+                c2=embedding_dim,
+                k=1,
+            )
 
         elif attention == 'backbone_subv1':
             self.lateral_c1 = ConvModule(c1_in_channels,768)
@@ -473,7 +488,19 @@ class SegFormerHead(nn.Module):
             sub_cca2 = self.sub_cca2(torch.cat([sub4, sub5], dim=1))
             cca_fuse = self.cca_fuse(torch.cat([sub_cca1, sub_cca2, sub6], dim=1))
             _c = self.linear_fuse(torch.cat([_c4, _c3, _c2, _c1, cca_fuse], dim=1))
+        elif self.attention == 'sub_multi_addv1':
+            p = 0.5
+            sub1 = _c1 * _c2 if random.random() < p else _c1 - _c2
+            sub2 = _c1 * _c3 if random.random() < p else _c1 - _c3
+            sub3 = _c1 * _c4 if random.random() < p else _c1 - _c4
+            sub4 = _c2 * _c3 if random.random() < p else _c2 - _c3
+            sub5 = _c2 * _c4 if random.random() < p else _c2 - _c4
+            sub6 = _c3 * _c4 if random.random() < p else _c3 - _c4
 
+            sub_cca1 = self.sub_cca1(torch.cat([sub1, sub2, sub3], dim=1))
+            sub_cca2 = self.sub_cca2(torch.cat([sub4, sub5], dim=1))
+            cca_fuse = self.cca_fuse(torch.cat([sub_cca1, sub_cca2, sub6], dim=1))
+            _c = self.linear_fuse(torch.cat([_c4, _c3, _c2, _c1, cca_fuse], dim=1))
         elif self.attention == 'backbone_subv1':
             # 先统一通道
             lateral_c1 = self.lateral_c1(c1)
