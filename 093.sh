dc47684f4d8ae14d94f2d2cd293b89f360e47bd5
yzc <yzc@qq.com>
2023-09-15 09:28:10 +0800

    add

--- 093.sh
+++ 093.sh
@@ -1,5 +1,5 @@
 
-CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -s 42  --config Drishti-GS/cropped_sup256x256/org_segformer/epochs100_warmup10e-2_lr1e-5_OHEM9e-1_backbone_b2_pretrained_flip_rotate_translate_CE_Loss
+CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -s 42  --config Drishti-GS/cropped_sup256x256/org_segformer/epochs100_warmup25e-2_OHEM9e-1_backbone_b2_pretrained_flip_rotate_translate_CE_Loss
 
 
 
