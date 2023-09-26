834da9b6259211a527b99dd84b4358aff5063a88
yzc <yzc@qq.com>
2023-09-26 19:41:25 +0800

    add

---
+++ v7-ii-1-6-v1.sh
@@ -0,0 +1,15 @@
+
+# loss
+# contrast
+CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -s 42  --config Drishti-GS/cropped_sup256x256/my_segformer/v7-ii-1-6-v1/contrast/backbone_b2_pretrained_flip_rotate_translate_scale_CE_2e-1FC_1e-1Contrast_Loss
+CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -s 42  --config Drishti-GS/cropped_sup256x256/my_segformer/v7-ii-1-6-v1/contrast/backbone_b2_pretrained_flip_rotate_translate_scale_CE_2e-1FC_2e-1Contrast_Loss
+CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -s 42  --config Drishti-GS/cropped_sup256x256/my_segformer/v7-ii-1-6-v1/contrast/backbone_b2_pretrained_flip_rotate_translate_scale_CE_2e-1FC_3e-1Contrast_Loss
+CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -s 42  --config Drishti-GS/cropped_sup256x256/my_segformer/v7-ii-1-6-v1/contrast/backbone_b2_pretrained_flip_rotate_translate_scale_CE_2e-1FC_4e-1Contrast_Loss
+CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -s 42  --config Drishti-GS/cropped_sup256x256/my_segformer/v7-ii-1-6-v1/contrast/backbone_b2_pretrained_flip_rotate_translate_scale_CE_2e-1FC_5e-1Contrast_Loss
+CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -s 42  --config Drishti-GS/cropped_sup256x256/my_segformer/v7-ii-1-6-v1/contrast/backbone_b2_pretrained_flip_rotate_translate_scale_CE_2e-1FC_6e-1Contrast_Loss
+CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -s 42  --config Drishti-GS/cropped_sup256x256/my_segformer/v7-ii-1-6-v1/contrast/backbone_b2_pretrained_flip_rotate_translate_scale_CE_2e-1FC_7e-1Contrast_Loss
+CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -s 42  --config Drishti-GS/cropped_sup256x256/my_segformer/v7-ii-1-6-v1/contrast/backbone_b2_pretrained_flip_rotate_translate_scale_CE_2e-1FC_8e-1Contrast_Loss
+CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -s 42  --config Drishti-GS/cropped_sup256x256/my_segformer/v7-ii-1-6-v1/contrast/backbone_b2_pretrained_flip_rotate_translate_scale_CE_2e-1FC_9e-1Contrast_Loss
+CUDA_VISIBLE_DEVICES=0 python main.py -ng 1 -s 42  --config Drishti-GS/cropped_sup256x256/my_segformer/v7-ii-1-6-v1/contrast/backbone_b2_pretrained_flip_rotate_translate_scale_CE_2e-1FC_10e-1Contrast_Loss
+
+
