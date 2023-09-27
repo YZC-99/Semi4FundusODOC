3c787528225c4e93c8981049bf2cb7285f84c215
yzc <yzc@qq.com>
2023-09-27 16:19:35 +0800

    add

--- v7-ii-1-6-v1.sh
+++ v7-ii-1-6-v1.sh
@@ -55,3 +55,7 @@ CUDA_VISIBLE_DEVICES=1 python main.py  --config Drishti-GS/cropped_sup256x256/my
 #  Dice
 CUDA_VISIBLE_DEVICES=0 python main.py  --config Drishti-GS/cropped_sup256x256/my_segformer/v7-ii-1-6-v1/loss/Dice/backbone_b2_pretrained_flip_rotate_translate_scale_CE_2DC_loss
 CUDA_VISIBLE_DEVICES=0 python main.py  --config Drishti-GS/cropped_sup256x256/my_segformer/v7-ii-1-6-v1/loss/Dice/backbone_b2_pretrained_flip_rotate_translate_scale_CE_DC_loss
+
+# IoU
+CUDA_VISIBLE_DEVICES=0 python main.py  --config Drishti-GS/cropped_sup256x256/my_segformer/v7-ii-1-6-v1/loss/IoU/backbone_b2_pretrained_flip_rotate_translate_scale_CE_IoU
+CUDA_VISIBLE_DEVICES=0 python main.py  --config Drishti-GS/cropped_sup256x256/my_segformer/v7-ii-1-6-v1/loss/IoU/backbone_b2_pretrained_flip_rotate_translate_scale_CE_2IoU
