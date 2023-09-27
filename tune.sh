7725681a136d28f69700ed5a078980fc8160d369
yzc <yzc@qq.com>
2023-09-27 16:21:51 +0800

    add

--- tune.sh
+++ tune.sh
@@ -3,3 +3,5 @@
 CUDA_VISIBLE_DEVICES=0 python tune_main.py -s 42  --config Drishti-GS/test_cfg/backbone_b2_pretrained_flip_rotate_translate_scale_CE_2e-1FC_6e-1Contrast_Loss
 CUDA_VISIBLE_DEVICES=0 python tune_main.py -s 42  --config Drishti-GS/test_cfg/backbone_b4_pretrained_flip_rotate_translate_scale_CE_2e-1FC_6e-1Contrast_Loss
 
+CUDA_VISIBLE_DEVICES=0 python tune_main.py -s 42  --config Drishti-GS/test_cfg/backbone_b2_pretrained_flip_rotate_translate_scale_CE_IoU
+
