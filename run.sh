942fd9faa7fba9bcd1f1c90260e7a499059788c4
yzc <yzc@qq.com>
2023-09-28 10:29:00 +0800

    add

--- run.sh
+++ run.sh
@@ -1,9 +1,5 @@
 (
-    CUDA_VISIBLE_DEVICES=0 python main.py --lr 10e-4 --backbone b4 --config Drishti-GS/cropped_sup256x256/my_segformer/v7-ii-1-6-v1/Base &
-    sleep 10  # 延迟1秒
-    CUDA_VISIBLE_DEVICES=1 python main.py --lr 9e-4 --backbone b4 --config Drishti-GS/cropped_sup256x256/my_segformer/v7-ii-1-6-v1/Base &
-    sleep 10  # 延迟1秒
-    CUDA_VISIBLE_DEVICES=2 python main.py --lr 8e-4 --backbone b4 --config Drishti-GS/cropped_sup256x256/my_segformer/v7-ii-1-6-v1/Base &
-    sleep 10  # 延迟1秒
-    CUDA_VISIBLE_DEVICES=3 python main.py --lr 7e-4 --backbone b4 --config Drishti-GS/cropped_sup256x256/my_segformer/v7-ii-1-6-v1/Base
+    CUDA_VISIBLE_DEVICES=0 python main.py --lr 6e-5 --warmup 1e-2 --config Drishti-GS/cropped_sup256x256/my_segformer/v7-ii-1-6-v1/Base &
+    sleep 5  # 延迟1秒
+    CUDA_VISIBLE_DEVICES=1 python main.py --lr 1e-3 --warmup 1e-2 --config Drishti-GS/cropped_sup256x256/my_segformer/v7-ii-1-6-v1/Base &
 )
