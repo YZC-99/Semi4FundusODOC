7217d6dafa141c84f9b5555d401ca437cfb328de
yzc <yzc@qq.com>
2023-09-28 08:10:12 +0800

    add

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
-CUDA_VISIBLE_DEVICES=0 python main.py  --config Drishti-GS/cropped_sup256x256/my_segformer/b3/flip_rotate_translate_scale_CE_2e-1FC_6e-1Contrast_Loss
+CUDA_VISIBLE_DEVICES=0 python main.py --config Drishti-GS/cropped_sup256x256/my_segformer/b2/flip_rotate_translate_scale_CE_2e-1FC_6e-1Contrast_Loss
+
+CUDA_VISIBLE_DEVICES=1 python main.py --config Drishti-GS/cropped_sup256x256/my_segformer/b2/flip_rotate_translate_scale_CE_2e-1FC_6e-1Contrast_Loss
