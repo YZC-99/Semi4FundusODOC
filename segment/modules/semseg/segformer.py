318b984ac67949855db888b4b8613d67292127e2
yzc <yzc@qq.com>
2023-10-01 13:12:06 +0800

    add

--- segment/modules/semseg/segformer.py
+++ segment/modules/semseg/segformer.py
@@ -903,17 +903,17 @@ class SegFormerHead(nn.Module):
 
         ############## MLP decoder on C1-C4 ###########
         n, _, h, w = c4.shape
-        
-        # _c4 = self.linear_c4(c4).permute(0,2,1).reshape(n, -1, c4.shape[2], c4.shape[3])
-        # _c4 = F.interpolate(_c4, size=c1.size()[2:], mode='bilinear', align_corners=False)
-        #
-        # _c3 = self.linear_c3(c3).permute(0,2,1).reshape(n, -1, c3.shape[2], c3.shape[3])
-        # _c3 = F.interpolate(_c3, size=c1.size()[2:], mode='bilinear', align_corners=False)
-        #
-        # _c2 = self.linear_c2(c2).permute(0,2,1).reshape(n, -1, c2.shape[2], c2.shape[3])
-        # _c2 = F.interpolate(_c2, size=c1.size()[2:], mode='bilinear', align_corners=False)
-        #
-        # _c1 = self.linear_c1(c1).permute(0,2,1).reshape(n, -1, c1.shape[2], c1.shape[3])
+        # if self.attention == 'org':
+        _c4 = self.linear_c4(c4).permute(0,2,1).reshape(n, -1, c4.shape[2], c4.shape[3])
+        _c4 = F.interpolate(_c4, size=c1.size()[2:], mode='bilinear', align_corners=False)
+
+        _c3 = self.linear_c3(c3).permute(0,2,1).reshape(n, -1, c3.shape[2], c3.shape[3])
+        _c3 = F.interpolate(_c3, size=c1.size()[2:], mode='bilinear', align_corners=False)
+
+        _c2 = self.linear_c2(c2).permute(0,2,1).reshape(n, -1, c2.shape[2], c2.shape[3])
+        _c2 = F.interpolate(_c2, size=c1.size()[2:], mode='bilinear', align_corners=False)
+
+        _c1 = self.linear_c1(c1).permute(0,2,1).reshape(n, -1, c1.shape[2], c1.shape[3])
 
         #
         if self.attention == 'subv1':
