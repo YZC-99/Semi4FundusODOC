7331ce848763e808fa602989f541dce0cbf43a62
yzc <yzc@qq.com>
2023-10-01 12:56:47 +0800

    add

--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
-CUDA_VISIBLE_DEVICES=0 main.py  --lr 1.0e-3 --warmup 0.25 --BD_loss 2.0 --FC_loss 0.0 --ContrastCrossPixelCorrect_loss 0.4 --epochs 100 --scheduler poly --config Drishti-GS/cropped_sup256x256/my_segformer/v7-ii-1-6-v1/noise --d "探究warmup0.25和学习率0.001是不是最优解"
+CUDA_VISIBLE_DEVICES=0 python main.py  --lr 3.0e-4 --warmup 0.15 --BD_loss 2.0 --FC_loss 0.1 --ContrastCrossPixelCorrect_loss 0.4 --epochs 100 --scheduler poly --config REFUGE/cropped_sup256x256/my_segformer/v7-ii-1-6-v1/noise --d "学习率" &
+sleep 15
+CUDA_VISIBLE_DEVICES=1 python main.py  --lr 4.0e-4 --warmup 0.15 --BD_loss 2.0 --FC_loss 0.1 --ContrastCrossPixelCorrect_loss 0.4 --epochs 100 --scheduler poly --config REFUGE/cropped_sup256x256/my_segformer/v7-ii-1-6-v1/noise --d "学习率" &
+sleep 15
+CUDA_VISIBLE_DEVICES=2 python main.py  --lr 5.0e-4 --warmup 0.15 --BD_loss 2.0 --FC_loss 0.1 --ContrastCrossPixelCorrect_loss 0.4 --epochs 100 --scheduler poly --config REFUGE/cropped_sup256x256/my_segformer/v7-ii-1-6-v1/noise --d "学习率"
