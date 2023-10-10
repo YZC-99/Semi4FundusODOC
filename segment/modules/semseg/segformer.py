1c9a507725460291da21cda8a6d6ea9b4a3df24e
yzc <yzc@qq.com>
2023-10-10 14:18:30 +0800

    add

--- segment/modules/semseg/segformer.py
+++ segment/modules/semseg/segformer.py
@@ -305,6 +305,28 @@ class SegFormerHead(nn.Module):
                 CrissCrossAttention(c1_in_channels),
                 ConvModule(c1_in_channels, 64, k=3, p=1)
             )
+        elif attention == 'o1-fam-inj-skip-no-FAM-IFM':
+            self.dam = DAM(c4_in_channels)
+
+            self.inj3 = Skip_InjectionMultiSum_Auto_pool(c3_in_channels,c3_in_channels,activations=nn.ReLU6)
+            self.inj2 = Skip_InjectionMultiSum_Auto_pool(c2_in_channels,c2_in_channels,activations=nn.ReLU6)
+            self.inj1 = Skip_InjectionMultiSum_Auto_pool(c1_in_channels,c1_in_channels,activations=nn.ReLU6)
+            self.ffn0 = nn.Sequential(
+                CrissCrossAttention(c4_in_channels),
+                ConvModule(c4_in_channels, c3_in_channels,k=3,p=1)
+            )
+            self.ffn1 = nn.Sequential(
+                CrissCrossAttention(c3_in_channels),
+                ConvModule(c3_in_channels, c2_in_channels,k=3,p=1)
+            )
+            self.ffn2 = nn.Sequential(
+                CrissCrossAttention(c2_in_channels),
+                ConvModule(c2_in_channels, c1_in_channels,k=3,p=1)
+            )
+            self.ffn3 = nn.Sequential(
+                CrissCrossAttention(c1_in_channels),
+                ConvModule(c1_in_channels, 64,k=3,p=1)
+            )
         elif attention == 'o1-fam-inj-cbam-skip':
             self.dam = DAM(c4_in_channels)
             self.low_FAM_IFM = FAMIFM(fusion_in=c2_in_channels + c1_in_channels + c3_in_channels + c4_in_channels,
@@ -493,6 +515,25 @@ class SegFormerHead(nn.Module):
             _c2 = self.inj1(_c2,global_c1,c1)
             _c1 = self.ffn3(_c2)
             out_feat = _c1
+        elif self.attention == 'o1-fam-inj-skip-no-FAM-IFM':
+            # global_info = self.low_FAM_IFM((c1,c2,c3,c4))
+            global_c1 = c1
+            global_c2 = c2
+            global_c3 = c3
+            c4 = self.dam(c4)
+            c4 = self.ffn0(c4)
+            _c4 = F.interpolate(c4, size=c3.size()[2:], mode='bilinear', align_corners=False)
+            _c4 = self.inj3(_c4,global_c3,c3)
+            c3 = self.ffn1(_c4)
+
+            _c3 = F.interpolate(c3, size=c2.size()[2:], mode='bilinear', align_corners=False)
+            _c3 = self.inj2(_c3, global_c2,c2)
+            c2 = self.ffn2(_c3)
+
+            _c2 = F.interpolate(c2, size=c1.size()[2:], mode='bilinear', align_corners=False)
+            _c2 = self.inj1(_c2,global_c1,c1)
+            _c1 = self.ffn3(_c2)
+            out_feat = _c1
         elif self.attention == 'o1-fam-inj-v1':
             global_info = self.low_FAM_IFM((c1,c2,c3,c4))
             global_c1 = global_info[0]
@@ -625,7 +666,7 @@ if __name__ == '__main__':
     # sd = torch.load(ckpt_path,map_location='cpu')
 
     # model = ResSegFormer(num_classes=3, phi='b2',res='resnet34', pretrained=False,version='v2')
-    model = SegFormer(num_classes=3, phi='b2', pretrained=False,attention='o1-fam-inj-skip-no-dam')
+    model = SegFormer(num_classes=3, phi='b2', pretrained=False,attention='o1-fam-inj-skip-no-FAM-IFM')
     img = torch.randn(2,3,256,256)
     out = model(img)
     logits = out['out']
