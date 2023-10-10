f503d0ebe054e61660822768d19f78696882e046
yzc <yzc@qq.com>
2023-10-10 23:48:07 +0800

    add

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ if __name__ == '__main__':
 
     parser.add_argument('-u', '--update_every', type=int, default=1)
     parser.add_argument('-a', '--use_amp', default=False, action='store_true')
-    parser.add_argument('--early_stop', default=False, action='store_true')
+    parser.add_argument('--early_stop', type=int,default=25)
     parser.add_argument('-gc', '--grad_clip', default=0.0, type=float)
 
     parser.add_argument('-b', '--batch_frequency', type=int, default=10000)
@@ -230,8 +230,8 @@ if __name__ == '__main__':
     if args.saw:
         SWA_callback = StochasticWeightAveraging(swa_lrs=cfg.MODEL.lr * 0.1, swa_epoch_start=50, annealing_epochs=50)
         callbacks.append(SWA_callback)
-    if args.early_stop:
-        early_stop_callback = EarlyStopping(monitor="val_loss", min_delta=0.00, patience=25, verbose=False,
+    if args.early_stop > 0:
+        early_stop_callback = EarlyStopping(monitor="val_loss", min_delta=0.00, patience=args.early_stop, verbose=False,
                                             mode="min")
         callbacks.append(early_stop_callback)
     # Build trainer
