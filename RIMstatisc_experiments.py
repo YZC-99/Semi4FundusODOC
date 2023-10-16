77322a305947e3e179cd831863aa694913e01c32
yzc <yzc@qq.com>
2023-10-16 11:37:19 +0800

    add

--- RIMstatisc_experiments.py
+++ RIMstatisc_experiments.py
@@ -2,5 +2,5 @@ import os
 import csv
 from utils.general import get_config_from_file
 from utils.log_metrics_write import logs2csv
-path = 'experiments/RIM-ONE/cropped_sup256x256'
+path = 'experiments/RIM-ONE/'
 logs2csv(path)
