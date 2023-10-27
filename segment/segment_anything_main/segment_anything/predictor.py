3582722ae7897b78364dd571ba65a7abf172c949
yzc <yzc@qq.com>
2023-10-27 18:28:58 +0800

    add

--- segment/segment_anything_main/segment_anything/predictor.py
+++ segment/segment_anything_main/segment_anything/predictor.py
@@ -7,7 +7,7 @@
 import numpy as np
 import torch
 
-from segment_anything.modeling import Sam
+from segment.segment_anything_main.segment_anything.modeling import Sam
 
 from typing import Optional, Tuple
 
